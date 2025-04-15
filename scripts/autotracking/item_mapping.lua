a22d66375424cc1bf28e4f9d51bbc73e58a1aaf1
fafale <olhoender@gmail.com>
2025-04-15 15:48:31 -0300

    Mapping mastery items

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -3,14 +3,24 @@
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
 BASE_ITEM_ID = 0
 ITEM_MAPPING = {
-	[BASE_ITEM_ID + 00000] = { { "toggle" } },
-	[BASE_ITEM_ID + 00001] = { { "progressive" } },
-	[BASE_ITEM_ID + 00002] = { { "consumable" } },
-	-- handle progressive_toggle as toggle, only changing it's active state
-	[BASE_ITEM_ID + 00003] = { { "progressive_toggle", "toggle" } },
-	-- multiple items on this id, add the consumable 3 times
-	[BASE_ITEM_ID + 00004] = { { "toggle" }, { "consumable", nil, 3 } }
+	-- Medals
+	[2]					= { "Medal", 				"consumable"},
 
-	[2]					= {"Medal", 		"consumable"},
-	[50]				= {"Mastery_DVa", 	"consumable"}
+	-- Hero Mastery
+	[45]				= { "Mastery_Mercy", 		"consumable"},
+	[46]				= { "Mastery_Reinhardt", 	"consumable"},
+	[47]				= { "Mastery_Tracer", 		"consumable"},
+	[48]				= { "Mastery_Sojourn", 		"consumable"},
+	[49]				= { "Mastery_Winston", 		"consumable"},
+	[50]				= { "Mastery_DVa", 			"consumable"},
+	[51]				= { "Mastery_Echo", 		"consumable"},
+	[52]				= { "Mastery_Genji", 		"consumable"},
+	[53]				= { "Mastery_Lucio", 		"consumable"},
+	[54]				= { "Mastery_Mei", 			"consumable"},
+	[55]				= { "Mastery_Soldier 76", 	"consumable"},
+	[56]				= { "Mastery_Kiriko", 		"consumable"},
+	[57]				= { "Mastery_Cassidy", 		"consumable"},
+	[58]				= { "Mastery_Brigitte", 	"consumable"},
+
+
 }
