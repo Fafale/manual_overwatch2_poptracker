492ae243cd733794619b4e9a04ebf74db73dc48e
fafale <olhoender@gmail.com>
2025-04-15 15:42:21 -0300

    Start of autotracking options

---
+++ scripts/autotracking/slot_data.lua
@@ -0,0 +1,13 @@
+function get_slot_options(slot_data)
+    if slot_data["include_hero_masteries"] ~= nil then
+        local obj = Tracker:FindObjectForCode("hard")
+        local stage = slot_data["include_hero_masteries"]
+        if stage == 0 then
+            obj.CurrentStage = 0
+        elseif stage == 1 then
+            obj.CurrentStage = 1
+        elseif stage == 2 then
+            obj.CurrentStage = 2
+        end
+    end
+end
