a3bd5f060de8dc73f951c1be27f109b4609131a7
fafale <olhoender@gmail.com>
2025-04-15 22:38:26 -0300

    Add deathmatch option tracking (possibly)

--- scripts/autotracking/slot_data.lua
+++ scripts/autotracking/slot_data.lua
@@ -1,19 +1,31 @@
 function get_slot_options(slot_data)
+    -- Hero Mastery
     if slot_data["include_hero_masteries"] ~= nil then
         local obj = Tracker:FindObjectForCode("HeroMastery")
         local stage = slot_data["include_hero_masteries"]
-        if stage == 0 then
-            obj.CurrentStage = 0
-        elseif stage == 1 then
-            obj.CurrentStage = 1
-        elseif stage == 2 then
-            obj.CurrentStage = 2
-        end
+
+        obj.CurrentStage = stage
     end
 
     if slot_data["hero_mastery_check_amount"] ~= nil then
         local obj = Tracker:FindObjectForCode("HeroMastery_CheckAmount")
         local stage = slot_data["hero_mastery_check_amount"]
+        obj.AcquiredCount = stage
+    end
+
+
+    -- Deathmatch
+
+    if slot_data["include_deathmatch_checks"] ~= nil then
+        local obj = Tracker:FindObjectForCode("Deathmatch")
+        local stage = slot_data["include_deathmatch_checks"]
+        
+        obj.CurrentStage = stage
+    end
+
+    if slot_data["deathmatch_check_amount"] ~= nil then
+        local obj = Tracker:FindObjectForCode("Deathmatch_CheckAmount")
+        local stage = slot_data["deathmatch_check_amount"]
         obj.AcquiredCount = stage
     end
 end
